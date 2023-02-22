d5d0e75cb0d5994ec2ea5ccb8e2d7b34be663ac0
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-22 15:29:33 +0100

    Writing More Complex JSX Code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,11 @@
 function ExpenseItem() {
   return (
     <div>
-      <h2>Expense item!</h2>
+      <div>March 28th 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
     </div>
   );
 }
