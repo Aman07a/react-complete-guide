371035ebdf1e73adf63d0e73ada5746eff25753c
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-27 20:53:37 +0100

    Starting Setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import ReactDOM from "react-dom/client";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
 
-import "./index.css";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+export default App;
